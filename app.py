9a9bd2fc08def3f3262f2323c10fdc4827ef0c14
unluckyghst <seu>
2024-11-07 17:20:13 -0300

    quase terminando

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-# Importanto a biblioteca do flask para fazer um site dinamico
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, flash, url_for
 app = Flask(__name__)
+app.secret_key = "cheve_muito_segura"
 
-# Definindo a rota principal do site
+@app.route('/esqueci-minha-senha')
+def esquecisenha1():
+    return render_template ('esqueci-minha-senha.html')
+
+@app.route('/esqueci-minha-senha2')
+def esquecisenha2():
+    return render_template('esqueci-minha senha2.html')
+
+@app.route('/galeria')
+def galeria():
+    return render_template('galeria.html')
+
+if __name__== '__main__':
+    app.run(debug=True)
+
+@app.route("/verificar-login", methods=["post"]) 
+def verificar_login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in usuarios and usuarios[username] == password :
+        return redirect(url_for('galeria'))
+    else:
+        flash('usuário')
+
+
+
+
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -32,7 +60,6 @@ def inicio():
 def configuração():
     return render_template('configuração.html')
 
-# Parte principal do programa em Python
 if __name__=='__main__':
     app.run(debug=True)
 
